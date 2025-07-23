37bbce7e8bd1b1e6850f980549a9c114126172e5
ferretasso <ferretasso@hotmail.com>
2025-07-23 18:51:06 +0000

    Criei o cadastro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,109 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
+int main(){
 
-printf("Desafio cadastro");
+//Carta 1
+char estado_1[2];
+char codigo_1[4];
+char cidade_1[33];
+int populacao_1;
+int area_1;
+int pib_1;
+int pontos_turisticos_1;
+
+printf("JOGO SUPERTRUNFO\n");
+printf("VAMOS PREENCHER OS DADOS DA CARTA 1:\n");
+printf("POR FAVOR PREENCHA OS DADOS A SEGUIR:\n");
+printf("INFORMAR A SIGLA DO ESTADO COM UMA LETRA, CONFORME TABELA ABAIXO\n");
+printf("A=AMAZONAS\nB=BAHIA\nC=CEARÁ\nD=RIO DE JANEIRO\nE=PARANÁ\nF=SANTA CATARINA\nG=ALAGOAS\n");
+printf("QUAL ESTADO VOCÊ ESCOLHE?");
+scanf("%s", &estado_1);
+
+printf("INFORMAR O CÓDIGO DA CARTA, CONFORME TABELA ABAIXO\n");
+printf("A01, B01 C01 OU D01\n");
+printf("QUAL CÓDIGO DE CARTA VOCÊ ESCOLHE?");
+scanf("%s", &codigo_1);
+
+printf("QUAL NOME DE CIDADE VOCÊ ESCOLHE?");
+scanf("%s", &cidade_1);
+
+printf("QUAL A POPULAÇÃO DA CIDADE?");
+scanf("%d", &populacao_1);
+
+printf("QUAL A ÁREA DA CIDADE EM METROS2?");
+scanf("%d", &area_1);
+
+printf("QUAL O PIB - PRODUTO INTERNO BRUTO DO ESTADO?");
+scanf("%d", &pib_1);
+
+printf("QUAL O NÚMERO DE PONTOS TURÍSTICOS DA CIDADE?");
+scanf("%d", &pontos_turisticos_1);
+
+printf("CARTA 1\n");
+printf("ESTADO: %s \n", estado_1);
+printf("CÓDIGO: %s \n", codigo_1);
+printf("CIDADE: %s \n", cidade_1);
+printf("POPULAÇÃO: %d \n", populacao_1);
+printf("ÁREA: %d \n", area_1);
+printf("PIB: %d \n", pib_1);
+printf("NÚMERO DE PONTOS TURÍSTICOS: %d \n", pontos_turisticos_1);
+
+printf("-------------------------------------------------------------------------\n");
+
+
+//Carta 2
+char estado_2[2];
+char codigo_2[4];
+char cidade_2[33];
+int populacao_2;
+int area_2;
+int pib_2;
+int pontos_turisticos_2;
+
+
+printf("VAMOS PREENCHER OS DADOS DA CARTA 2:\n");
+printf("POR FAVOR PREENCHA OS DADOS A SEGUIR:\n");
+printf("INFORMAR A SIGLA DO ESTADO COM UMA LETRA, CONFORME TABELA ABAIXO\n");
+printf("A=AMAZONAS\nB=BAHIA\nC=CEARÁ\nD=RIO DE JANEIRO\nE=PARANÁ\nF=SANTA CATARINA\nG=ALAGOAS\n");
+printf("QUAL ESTADO VOCÊ ESCOLHE?");
+scanf("%s", &estado_2);
+printf("INFORMAR O CÓDIGO DA CARTA, CONFORME TABELA ABAIXO\n");
+printf("A01, B01 C01 OU D01\n");
+printf("QUAL CÓDIGO DE CARTA VOCÊ ESCOLHE?");
+scanf("%s", &codigo_2);
+
+printf("QUAL NOME DE CIDADE VOCÊ ESCOLHE?");
+scanf("%s", &cidade_2);
+
+printf("QUAL A POPULAÇÃO DA CIDADE?");
+scanf("%d", &populacao_2);
+
+printf("QUAL A ÁREA DA CIDADE EM METROS2?");
+scanf("%d", &area_2);
+
+printf("QUAL O PIB - PRODUTO INTERNO BRUTO DO ESTADO?");
+scanf("%d", &pib_2);
+
+printf("QUAL O NÚMERO DE PONTOS TURÍSTICOS DA CIDADE?");
+scanf("%d", &pontos_turisticos_2);
+
+printf("CARTA 2\n");
+printf("ESTADO: %s \n", estado_2);
+printf("CÓDIGO: %s \n", codigo_2);
+printf("CIDADE: %s \n", cidade_2);
+printf("POPULAÇÃO: %d \n", populacao_2);
+printf("ÁREA: %d \n", area_2);
+printf("PIB: %d \n", pib_2);
+printf("NÚMERO DE PONTOS TURÍSTICOS: %d \n", pontos_turisticos_2);
+
+
+return 0;
+
+}
+
+
+
 
     
-}
+
